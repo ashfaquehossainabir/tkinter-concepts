332b899f36e9b8d7ff5b37673773b61efb359e7f
Ashfaque Hossain Abir <109417020+ashfaquehossainabir@users.noreply.github.com>
2024-07-15 12:06:32 +0600

    Uploaded

---
+++ concept-10.py
@@ -0,0 +1,20 @@
+from tkinter import *
+from tkinter import colorchooser
+
+# Changing window background color concept
+
+
+def click():
+    color = colorchooser.askcolor()
+    window.config(bg=color[1])  # ColorHex = color[1]
+
+
+window = Tk()
+window.geometry("420x420")
+
+button = Button(text="Click me!", command=click)
+button.pack()
+
+window.mainloop()
+
+# Continue from 1:37:14
